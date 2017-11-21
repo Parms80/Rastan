32a21b0da698604a532c9136af67a77d68e01fc2
Perminder Suman <permindersuman@Perminders-MBP.lan>
2017-11-21 21:11:46 +0000

    First commit of all files

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+	public float jumpStrength;
+	//	private Rigidbody2D rigidBody;
+	private int state;
+	private bool grounded;
+	private Transform groundCheck;
+
+	// Use this for initialization
+	void Start () {
+		//		rigidBody = GetComponent<Rigidbody2D> ();
+		groundCheck = transform.Find("Ground check");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		grounded = isCharacterOnGround();
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.Space) && isCharacterOnGround()) {
+			GetComponent<Rigidbody2D> ().AddForce(new Vector2(0f, jumpStrength));
+		}
+	}
+
+	bool isCharacterOnGround()
+	{
+		bool isOnGround = Physics2D.Linecast(transform.position, 
+			groundCheck.position, 
+			1 << LayerMask.NameToLayer("Ground"));
+		Debug.Log ("isOnGround = " + isOnGround);
+		return isOnGround;
+	}
+}
---
+++ Assets/SmoothFollow2.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow2 : MonoBehaviour {
+	
+	public Transform target;
+	public float distance = 3.0f;
+	public float height = 3.0f;
+	public float damping = 5.0f;
+	public bool smoothRotation = true;
+	public float rotationDamping = 10.0f;
+	public bool lockRotation;
+	public bool follow;
+	public bool lockHeight;
+
+	// Use this for initialization
+	void Start () {
+		follow = true;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 wantedPosition = target.TransformPoint(0, height, -distance);
+
+		if (!follow)
+		{
+			// Only stop the horizontal scrolling
+			wantedPosition.x = transform.position.x;
+		}
+
+		transform.position = Vector3.Lerp (transform.position, wantedPosition, Time.deltaTime * damping);
+
+//		if (lockHeight)
+//		{
+//			transform.position.y = height;
+//		}
+
+		if (smoothRotation) {
+			Quaternion wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
+			transform.rotation = Quaternion.Slerp (transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
+		}
+		else
+		{
+			transform.LookAt (target, target.up);
+		}
+
+
+		if (lockRotation)
+		{
+			transform.localRotation = Quaternion.EulerAngles(0,0,0);
+		}	
+	}
+
+	void SetFollow(bool flag)
+	{
+		follow = flag;
+	}
+}
